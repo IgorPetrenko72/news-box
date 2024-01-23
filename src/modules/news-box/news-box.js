f179a81ddf2154fbb5633a109214f9e3a0611068
IgorPetrenko72 <i.v.petrenko@gmail.com>
2024-01-23 23:03:19 +0100

    webpack

--- src/modules/news-box/news-box.js
+++ src/modules/news-box/news-box.js
@@ -1,19 +1,40 @@
-import moment from 'moment';
-import ApiArticlesService from '../service/articles';
-import articleTpl from './templates/article.hbs'
-
-const apiArticleService = new ApiArticlesService();
+import articlesService from '../service/articles';
+import notifier from '../service/notifier';
+import articleTpl from './templates/article.hbs';
 
 class NewsBox {
   #markup = `
-  <section class="news-app">
+    <section class="news-app">
     <section class="header">
-        <div class='header__wrapper'>
-            <a class='header__logo'>
-                <i class="fa-regular fa-newspaper"></i>
-            </a>
-            <div class='header__brand'>News Box</div>
-        </div>
+
+    <div class="inner-header flex">
+      <svg version="1.1" class="logo" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 500 500" xml:space="preserve">
+        <path fill="#FFFFFF" stroke="#000000" stroke-width="10" stroke-miterlimit="10" d="M57,283" />
+          <g><path fill="#fff"
+          d="M250.4,0.8C112.7,0.8,1,112.4,1,250.2c0,137.7,111.7,249.4,249.4,249.4c137.7,0,249.4-111.7,249.4-249.4
+          C499.8,112.4,388.1,0.8,250.4,0.8z M383.8,326.3c-62,0-101.4-14.1-117.6-46.3c-17.1-34.1-2.3-75.4,13.2-104.1
+          c-22.4,3-38.4,9.2-47.8,18.3c-11.2,10.9-13.6,26.7-16.3,45c-3.1,20.8-6.6,44.4-25.3,62.4c-19.8,19.1-51.6,26.9-100.2,24.6l1.8-39.7		c35.9,1.6,59.7-2.9,70.8-13.6c8.9-8.6,11.1-22.9,13.5-39.6c6.3-42,14.8-99.4,141.4-99.4h41L333,166c-12.6,16-45.4,68.2-31.2,96.2	c9.2,18.3,41.5,25.6,91.2,24.2l1.1,39.8C390.5,326.2,387.1,326.3,383.8,326.3z" />
+          </g>
+      </svg>
+      <h1>News Waves</h1>
+    </div>
+
+    <div>
+      <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+        <defs>
+          <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+        </defs>
+          <g class="parallax">
+            <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+            <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+            <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+            <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+          </g>
+      </svg>
+    </div>
+        
     </section>
     <form class="search">
         <div class="search__wrapper">
@@ -26,157 +47,126 @@ class NewsBox {
             </button>
         </div>
     </form>
-    <div class="articles"></article> 
-    </div>
-    <section class="load-more">
-    <button class="load-more__btn" hidden>More</button>
+      <div class="articles"></div> 
+      </div>
+      <section class="load-more">
+        <button class="load-more__btn load-more__btn_hidden">More</button>
+      </section>
     </section>
-    </section>
-  `;
+    `;
   #targetElement = null;
+  #infinityLoading = false;
+  #searchQuery = null;
   #refs = {};
   #articles = [];
-  #articleService = () => {};
 
-  constructor({ targetElement, articlesService } = {}) {
+  constructor({ targetElement, infinityLoading = false } = {}) {
     this.#targetElement = targetElement || document.body;
-    this.#articleService = articlesService;
+    this.#infinityLoading = infinityLoading;
   }
 
   init() {
     this.#targetElement.innerHTML = this.#markup;
-      this.#initRefs();
-      this.#initListeners();
+    this.#initRefs();
+    this.#initListeners();
+
+    if (this.#infinityLoading) {
+      this.#initInfinityLoading();
+    }
   }
 
   #initRefs() {
     this.#refs.search = document.querySelector('.news-app .search');
     this.#refs.articles = document.querySelector('.news-app .articles');
- }
+    this.#refs.moreBtn = document.querySelector('.news-app .load-more__btn');
+  }
 
   #initListeners() {
     this.#refs.search.addEventListener('submit', this.#onSearch.bind(this));
+    this.#refs.moreBtn.addEventListener('click', this.#onClickLoadMoreBtn.bind(this));
   }
 
   #updateArticles(articles) {
     this.#articles = articles;
     this.#render();
+
+    if (!this.#infinityLoading) {
+      this.#toggleMoreButton();
+    }
   }
 
   #onSearch(e) {
-     e.preventDefault();
-  const searchQuery = e.currentTarget.elements.search.value;
-  
-  this.#articleService
-    .fetchData(searchQuery)
-    .then((articles) => {
-      this.#updateArticles(articles)
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-    .finally(() => {
-      e.target.reset();
+    e.preventDefault();
+    this.#searchQuery = e.currentTarget.elements.search.value;
+
+    this.#fetchArticles()
+      .then((articles) => {
+        if (articles.length === 0) {
+          return notifier.info('No results. Please clarify your search');
+        }
+
+        this.#updateArticles(articles);
+      })
+      .finally(() => {
+        e.target.reset();
+      });
+  }
+
+  #fetchArticles() {
+    return articlesService
+      .fetchData(this.#searchQuery)
+      .then((articles) => articles)
+      .catch((error) => {
+        console.error(error); // dev
+        notifier.error('Something went wrong. Please try later'); //
+      });
+  }
+
+  #loadMore() {
+    return this.#fetchArticles().then((articles) => {
+      this.#updateArticles([...this.#articles, ...articles]);
     });
   }
 
-  #render() {
-     const mockup = this.#articles
-      .map(({ author, title, url, urlToImage, publishedAt, content }) => {
-        const { text, timeForReading } = parseContent(content);
+  #onClickLoadMoreBtn() {
+    this.#refs.moreBtn.classList.add('load-more__btn_loading');
+    this.#refs.moreBtn.disabled = true;
 
-        return articleTpl({
-          author,
-          title,
-          url,
-          content,
-          text,
-          urlToImage,
-          timeForReading,
-          publishDate: moment(publishedAt).format('lll'),
-        });
-      })
-      .join('');
+    this.#loadMore().finally(() => {
+      this.#refs.moreBtn.classList.remove('load-more__btn_loading');
+      this.#refs.moreBtn.disabled = false;
+    });
+  }
+
+  #toggleMoreButton() {
+    if (this.#articles.length > 0) {
+      this.#refs.moreBtn.classList.remove('load-more__btn_hidden');
+    } else {
+      this.#refs.moreBtn.classList.add('load-more__btn_hidden');
+    }
+  }
+
+  #initInfinityLoading() {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        for (const entry of entries) {
+          if (entry.isIntersecting && this.#articles.length > 0) {
+            this.#loadMore();
+          }
+        }
+      },
+      { rootMargin: '400px' },
+    );
+
+    observer.observe(this.#refs.moreBtn);
+  }
+
+  #render() {
+    const mockup = this.#articles.map((data) => articleTpl({ ...data })).join('');
 
     this.#refs.articles.innerHTML = mockup;
   }
 }
-// const apiArticlesService = new ApiArticlesService()
-// const markup = `
-//
-//     `;
-// add to DOM
-// document.body.innerHTML = markup;
 
-// const ref = {
-//   search: document.querySelector('.news-app .search'),
-//   articles: document.querySelector('.news-app .articles'),
-// };
-
-// ref.search.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   const searchQuery = e.currentTarget.elements.search.value;
-  
-//   apiArticlesService
-//     .fetchData(searchQuery)
-//     .then((articles) => {
-//     renderArticles(articles);
-//     })
-//     .finally(() => {
-//       e.target.reset();
-//     });
-  
-// });
-
-// function renderArticles(articles) {
-//   const markup = articles.map(({ author, title, url, urlToImage, publishedAt, content }) => {
-//     const { text, timeForReading } = parseContent(content);
-
-//     return `
-//     <article class="article">
-//       <h2 class="article__title">${title}</h2>
-//       <div class="article__publish-date">${moment(publishedAt).format('lll')}</div>
-//       <div class="article__author">${author}</div>
-//       <figure class="article__image">
-//         <img src="${urlToImage}" alt="article__image">
-//       </figure>
-//       <div class="article__content">
-//         ${text}
-//         <a href="${url}" target="_blank">read more</a>
-//       </div>
-//       <div class="article__time-for-reading">${timeForReading} for reading</div>
-//     </article>
-//     `;
-//   })
-//     .join('');
-//   ref.articles.innerHTML = markup;
-// };
-
-const newsBox = new NewsBox({articleService: new ApiArticlesService()});
+const newsBox = new NewsBox({ infinityLoading: true });
 newsBox.init();
-
-function parseContent(content) {
-  const firstSeparatorPosition = content.indexOf('[');
-  const secondSeparatorPosition = content.indexOf(']');
-  const parseResult = {
-    text: null,
-    timeForReading: null,
-  };
-  if (firstSeparatorPosition >= 0 && secondSeparatorPosition >= 0) {
-    const numberOfChars = parseInt(content.substring(firstSeparatorPosition + 1, secondSeparatorPosition));
-    parseResult.text = content.substring(0, firstSeparatorPosition);
-    parseResult.timeForReading = calculateTimeForReading(numberOfChars);
-  }
-}
-
-function calculateTimeForReading(numberOfChars) {
-  const minutes = Math.floor(numberOfChars / 255);
-  if (minutes === 0) {
-    return 'less then one minute';
-  }
-  if (minutes > 0 && minutes < 2) {
-    return 'more then one minute';
-  }
-  return `${minutes} minutes`;
-}
-
--- src/modules/service/articles.js
+++ src/modules/service/articles.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 const BASE_URL = 'https://newsapi.org/v2';
 const API_KEY = 'f709789d6e894d2ca4072f45d44a83bb';
 const OPTIONS = {
@@ -5,19 +7,77 @@ const OPTIONS = {
       Authorization: API_KEY,
     },
   };
-export default class ArticlesService {
-    fetchData(searchQuery) {
+class ArticlesService {
+    #page = 1;
+  #previousQuery = null;
+
+  fetchData(searchQuery) {
+    this.#page = searchQuery === this.#previousQuery ? this.#page + 1 : 1;
+    this.#previousQuery = searchQuery;
+
     const searchParams = new URLSearchParams({
-    q: searchQuery,
-    sortBy: 'publishedAt',
-    pageSize: 10,
-  })
+      q: searchQuery,
+      sortBy: 'publishedAt',
+      pageSize: 10,
+      page: this.#page,
+    });
 
     return fetch(`${BASE_URL}/everything?${searchParams}`, OPTIONS)
-    .then((response) => response.json())
-    .then(({articles}) => articles)
-    .catch((error) => {
-      console.log(error);
+      .then((response) => response.json())
+      .then(({ articles }) => ArticlesService.prepareData(articles))
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  static prepareData(articles) {
+    return articles.map((article) => {
+      const { author, title, content, url, urlToImage, publishedAt } = article;
+      const { text, timeForReading } = ArticlesService.parseContent(content);
+
+      return {
+        url,
+        text,
+        author,
+        title,
+        urlToImage,
+        timeForReading,
+        publishDate: moment(publishedAt).format('lll'),
+      };
     });
+  }
+
+  static parseContent(content) {
+    const firstSeparatorPosition = content.indexOf('[');
+    const secondSeparatorPosition = content.indexOf(']');
+    const parseResult = {
+      text: null,
+      timeForReading: null,
+    };
+
+    if (firstSeparatorPosition >= 0 && secondSeparatorPosition >= 0) {
+      const numberOfChars = parseInt(content.substring(firstSeparatorPosition + 1, secondSeparatorPosition));
+
+      parseResult.text = content.substring(0, firstSeparatorPosition);
+      parseResult.timeForReading = ArticlesService.calculateTimeForReading(numberOfChars);
     }
+
+    return parseResult;
+  }
+
+  static calculateTimeForReading(numberOfChars) {
+    const minutes = Math.floor(numberOfChars / 255);
+
+    if (minutes === 0) {
+      return 'less then one minute';
+    }
+
+    if (minutes > 0 && minutes < 2) {
+      return 'more then one minute';
+    }
+
+    return `${minutes} minutes`;
+  }
 }
+
+export default new ArticlesService();
